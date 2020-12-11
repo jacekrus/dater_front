67b2dd3a3ac3317bdbba9292df59ab441fed5649
jacekrus <jacek1807@gmail.com>
2020-12-11 15:56:41 +0100

    Changed base axios request URL to Heroku

--- src/AxiosRequest.js
+++ src/AxiosRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const config = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://dater-back.herokuapp.com',
     withCredentials: true,
     timeout: 8000
 })
