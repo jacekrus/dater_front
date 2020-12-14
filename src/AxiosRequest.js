d79be8e78964e4acbaaa599fa10d91c682a8961a
jacekrus <jacek1807@gmail.com>
2020-12-14 20:01:33 +0100

    Changed base axios url to secure https

--- src/AxiosRequest.js
+++ src/AxiosRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const config = axios.create({
-    baseURL: 'http://www.testapptgst.tk',
+    baseURL: 'https://www.testapptgst.tk',
     withCredentials: true,
     timeout: 8000
 })
